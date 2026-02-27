3bf734d0a4286360c985fe7952690972127c47d7
Thiru-Vikram <thiruvikramsde@gmail.com>
2026-02-27 20:59:31 +0530

    for loop in 1143

--- DP/Strings/Leetcode1143.java
+++ DP/Strings/Leetcode1143.java
@@ -72,8 +72,8 @@ public class Leetcode1143 {
         int m = text2.length();
         int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
 
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
