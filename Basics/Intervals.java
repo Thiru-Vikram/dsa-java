05c0c4d2d62a7397f64a6f7592086ba1183fb678
Thiru-Vikram <thiruvikramsde@gmail.com>
2026-03-10 19:12:12 +0530

    intervals

--- Basics/Intervals.java
+++ Basics/Intervals.java
@@ -4,7 +4,71 @@ import java.util.*;
 
 public class Intervals {
 
+    // tc and sc is o(n)
+    // insert intervals func
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        int n = intervals.length;
+        int i = 0;
+        List<int[]> res = new ArrayList<>();
+
+        // Add intervals that come before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
+        }
+
+        // Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+
+        // Add remaining intervals
+        while (i < n) {
+            res.add(intervals[i]);
+            i++;
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+
+    // merge intervals func
+    // int[][] activities = {{1,4}, {2,3}, {5,6}, {9,10}};
+    // Selected Activities: [1, 4] [5, 6] [9, 10]
+    // tc is (n log n) - sort + o(n) loop = o(n log n)
+    // sc is o(log n) arrays.sort uses extra space + o(n) for list = o(n)
+    public int[][] merge(int[][] intervals) {
+
+        // sort 1st ele
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+        result.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            // get prev from result
+            int[] last = result.get(result.size() - 1);
+            int[] curr = intervals[i];
+
+            // if curr start is <= prev end -> merge
+            if (curr[0] <= last[1]) {
+                last[1] = Math.max(last[1], curr[1]);
+                // else add it sepearte straight
+            } else {
+                result.add(curr);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
     // remove intervals func
+    // int[][] activities = {{1,3}, {2,5}, {4,6}, {6,7}};
+    // Selected Activities: [1, 3] [4, 6] [6, 7]
+    // tc is o(n log n) sc is o(n)
     public static List<int[]> activitySelection(int[][] activities) {
         Arrays.sort(activities, (a, b) -> a[1] - b[1]);
 
@@ -20,29 +84,5 @@ public class Intervals {
         }
         return result;
     }
-    // int[][] activities = {{1,3}, {2,5}, {4,6}, {6,7}};
-    // Selected Activities: [1, 3] [4, 6] [6, 7]
-
-    // merge intervals func
-    public static List<int[]> activitySelection2(int[][] activities) {
-        Arrays.sort(activities, (a, b) -> a[0] - b[0]);
-
-        List<int[]> result = new ArrayList<>();
-        result.add(activities[0]);
-
-        for (int i = 1; i < activities.length; i++) {
-            int[] last = result.get(result.size() - 1);
-            int[] curr = activities[i];
-
-            if (curr[0] <= last[1]) {
-                last[1] = Math.max(last[1], curr[1]);
-            } else {
-                result.add(curr);
-            }
-        }
-        return result;
-    }
-    // int[][] activities = {{1,4}, {2,3}, {5,6}, {9,10}};
-    // Selected Activities: [1, 4] [5, 6] [9, 10]
 
 }
