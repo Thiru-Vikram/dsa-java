e53d597cbc2d505f2b7cf5a83142b953c4c22afe
Thiru-Vikram <thiruvikramsde@gmail.com>
2026-06-30 22:29:32 +0530

    Non-overlapping Intervals

---
+++ Neetcode150/Intervals/Leetcode435.java
@@ -0,0 +1,47 @@
+package Neetcode150.Intervals;
+
+import java.util.*;
+
+// q is return the cnt of the no of overlapping intervals
+// a is just check the curr interval start time < prev interval end time 
+// then it overlaps cnt ++ else update prev to curr end time
+
+// 435. Non-overlapping Intervals
+public class Leetcode435 {
+
+    // tc is o(n log n) sc is o(n)
+    public static int eraseOverlapIntervals(int[][] intervals) {
+
+        int n = intervals.length;
+        int ans = 0;
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        int prev = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            int curr = intervals[i][0];
+            if (curr < prev) {
+                ans++;
+            } else {
+                prev = intervals[i][1];
+            }
+
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[][] grid = {
+                { 1, 2 },
+                { 2, 3 },
+                { 4, 5 },
+                { 5, 6 }
+        };
+
+        System.out.print(eraseOverlapIntervals(grid));
+
+    }
+
+}
